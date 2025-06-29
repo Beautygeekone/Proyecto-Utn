65f0632c9af96e5159c510636f5aa6a32b795249
beautygeekone <luciamccartney24@gmail.com>
2025-06-28 21:43:09 -0300

    vite

---
+++ src/Screens/Components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+import { CiMenuKebab } from "react-icons/ci";
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+  const links = [
+    { id: 1, link: 'Home' },
+    { id: 2, link: 'How We Work' },
+    { id: 3, link: 'Benefits' },
+    { id: 4, link: 'Services' },
+    { id: 5, link: 'Contact' },
+  ];
+
+  return (
+    <div className={styles.Navbar}>
+      <p>Ballerina Capuccina</p>
+      <CiMenuKebab onClick={() => setNavbarOpen(!navbarOpen)} />
+      {navbarOpen && (
+        <ul>
+          {links.map((x) => (
+            <li key={x.id}>
+              <a href="#">{x.link === "How We Work" ? "How we work" : x.link}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
