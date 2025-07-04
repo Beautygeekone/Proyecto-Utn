b4e65536f2213ffd30d6207cd4479071df3b05d7
beautygeekone <luciamccartney24@gmail.com>
2025-07-04 16:35:45 -0300

    svg

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -4,10 +4,48 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div className={styles.home}> 
-      <p>Lo que tenemos para ofrecerte</p>
-      <p>Productos de calidad</p>
-    </div>
+   <div className={styles.home}> 
+  <svg viewBox="0 0 1000 200" className={styles.wavySubtitle}>
+    <path
+      id="wavyPath1"
+      d="M 0 100 Q 250 50 500 100 T 1000 100"
+      fill="transparent"
+    />
+    <text className={styles.svgTextOnPath}>
+      <textPath href="#wavyPath1" startOffset="50%">
+        La vita è quella cosa che inizia dopo il caffè!
+           <animate
+        attributeName="startOffset"
+        from="0%"
+        to="100%"
+        dur="15s"
+        repeatCount="indefinite"
+      /> 
+      </textPath>
+    </text>
+  </svg>
+
+  <svg viewBox="0 0 1000 200" className={styles.wavySubtitle}>
+    <path
+      id="wavyPath2"
+      d="M 0 100 Q 250 50 500 100 T 1000 100"
+      fill="transparent"
+    />
+    <text className={styles.svgTextOnPath}>
+      <textPath href="#wavyPath2" startOffset="50%">
+        Los mejores productos los vas a encontrar aquí.
+          <animate
+        attributeName="startOffset"
+        from="100%"
+        to="0%"
+        dur="15s"
+        repeatCount="indefinite"
+      />
+      </textPath>
+    </text>
+  </svg>
+</div>
+
   )
 }
 
--- src/Screens/Stores.jsx
+++ src/Screens/Stores.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Benefits = () => {
+const Stores = () => {
   return (
     <div></div>
   )
 }
 
-export default Benefits
+export default Stores
