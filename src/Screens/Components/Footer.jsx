385e0f6ec1bb1036135e19fec2a04e79b9529402
beautygeekone <luciamccartney24@gmail.com>
2025-07-08 20:19:15 -0300

    products

--- src/Screens/Components/Footer.jsx
+++ src/Screens/Components/Footer.jsx
@@ -8,10 +8,11 @@ const Footer = () => {
       padding: '2rem',
       textAlign: 'center',
     }}>
-      <h2 style={{ margin: '0 0 1rem' }}>Ballerina Capuccina</h2>
+      <h2 style={{ margin: '0 0 2.5rem' }}>Ballerina Capuccina</h2>
       <p>Creando experiencias extraordinarias desde 1996.</p>
       <p>Email: contacto@ballerinacapuccina.com  |  Celular: +54 (0291) 154097654</p>
-      <p style={{ marginTop: '1rem', fontSize: '0.9rem', color: '#aaa' }}>
+      <p>Tiendas: Av. Libertador 1234, Buenos Aires, Argentina | Duarte Quirós 1400,  Córdoba, Argentina.</p>
+      <p style={{ marginTop: '3rem', fontSize: '0.9rem', color: '#aaa', }}>
         &copy; {new Date().getFullYear()} Ballerina Capuccina. Todos los derechos reservados.
       </p>
     </footer>
